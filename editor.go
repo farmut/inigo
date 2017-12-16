32850bb89ebb754aea327130a0a08f94d89d95c6
hIMEI <hIMEI@tuta.io>
2017-12-16 12:14:02 -0500

    editor.go is written

--- editor.go
+++ editor.go
@@ -15,3 +15,49 @@
 /////////////////////////////////////////////////////
 
 package inigo
+
+import (
+	"bufio"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+type Editor struct {
+
+	// Boolean const
+	ENABLED Nbld
+
+	// All file's content. Empty strings included
+	Content []string
+}
+
+func (e Editor) EditFile(filename string) []string {
+	//e.Content = make([]string)
+	file, err := os.OpenFile(filename, os.O_RDWR, 0755)
+
+	ErrFatal(err)
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		e.Content = append(e.Content, scanner.Text())
+	}
+
+	return e.Content
+}
+
+func (e Editor) enableParam(paramstring string) {
+	for i, str := range e.Content {
+		if strings.Contains(str, paramstring) {
+			e.Content[i] = strings.TrimPrefix(e.Content[i], COMM)
+		}
+	}
+
+	newContent := strings.Join(e.Content, "\n")
+	err := ioutil.WriteFile(FILENAME, []byte(newContent), 0644)
+
+	ErrFatal(err)
+}
--- example/example.go
+++ example/example.go
@@ -4,7 +4,7 @@
  * @Author: hIMEI
  * @Date:   2017-12-13 17:49:49
  * @Last Modified by:   hIMEI
- * @Last Modified time: 2017-12-15 20:30:03
+ * @Last Modified time: 2017-12-16 12:11:12
  *
  */
 
@@ -37,7 +37,7 @@ func main() {
 	//param := ini.GetParamByName("[SECTION1]", "angryByrds")
 
 	fmt.Println("___")
-	value := ini.GetValue("[SECTION1]", "param5")
+	value := ini.GetValue("[SECTION1]", "param7")
 	fmt.Println(value)
 
 	/*
